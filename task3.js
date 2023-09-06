465ef5003f4df9942670b6c2219852be1917a748
Deividas-BlueSkin <somecrazyp@gmail.com>
2023-09-06 15:29:14 +0300

    commit

---
+++ task3.js
@@ -0,0 +1,43 @@
+
+function dayConvert(days, format) {
+    let num
+    if (format == 'min') { num = days * 24 * 60 }
+    if (format == 'hour') { num = days * 24 }
+    if (format == 'week') { Math.floor(num = days / 7) }
+    if (format == 'month') { Math.floor(mun = days / 30.4) }
+    if (format == 'year') { Math.floor(num = days / 365) }
+    return `${days} days --> ${num} ${format}s`
+}
+
+console.log(dayConvert(50, 'min'))
+
+function checkFraction(num1, num2) {
+    let output
+    let liek = num1 % num2
+    if (liek == 0) { output = `${num1} dalijas is ${num2}` }
+    else { output = `${num1} nesidalija is ${num2}, liekana ${liek}` }
+    return output
+}
+
+console.log(checkFraction(13, 10))
+
+function checkText(text = 'text') {
+    let por = (text.length % 2 == 0)
+    return por
+}
+
+console.log(checkText('bingCho'))
+
+function pickChar(text, num) {
+    let Length = text.length
+    if (Length >= Math.abs(num)) {
+        if (num < 0) { num = Length + num +1}
+        char = text[num - 1]
+        return `${text} ${num} character is ${char}`
+    }
+    else {
+        return `ERROR: ${text} has ${Length} symbols, your number is too big`
+    }
+}
+
+console.log(pickChar('Donuthor', -8))
